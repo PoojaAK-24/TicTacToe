064cffc089bb329e3ada1ab3cc6f3363e3b86ad7
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-21 11:40:10 +0530

    Pooja Added:Initializing Empty Board

---
+++ UC1_InitializeBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class UC1_InitializeBoard
+    {
+        public char[] Board()
+        {
+            char[] CreateBoard = new char[10];
+            for (int i = 1; i < CreateBoard.Length; i++)
+            {
+                CreateBoard[i] = ' ';
+            }
+            Console.WriteLine("Board Initialized");
+            return CreateBoard;
+        }
+    }
+}
